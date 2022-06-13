da82e5f01660f389bee3a7ba8067d947f3d2894f
Vuong Pham <macbookair@Macbooks-MacBook-Air.local>
2022-06-13 17:04:53 +0700

    user group - brand & design firm list + table component

---
+++ src/pages/TISC/UserGroup/Brand/services/api.ts
@@ -0,0 +1,28 @@
+import { request } from 'umi';
+import { message } from 'antd';
+import type { IBrandListResponse } from '../types';
+import type { IDataTableResponse } from '@/components/Table/index';
+
+export async function getBrandPagination(
+  params: any,
+  callback: (data: IDataTableResponse) => void,
+) {
+  request(`/api/brand/get-list`, {
+    method: 'GET',
+    params,
+  })
+    .then((response: IBrandListResponse) => {
+      callback({
+        data: response.data,
+        pagination: {
+          current: params.page,
+          pageSize: params.pageSize,
+          total: 10,
+        },
+      });
+    })
+    .catch((error) => {
+      console.log('error', error);
+      message.error(error.message);
+    });
+}
