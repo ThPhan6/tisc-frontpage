1b28051b2cae62ce8bb47a2b5c40d3c37625e763
VuongPham96 <vuongd36@gmail.com>
2022-07-04 10:33:38 +0700

    create agreement policies terms entry form

--- config/routes.ts
+++ config/routes.ts
@@ -244,7 +244,8 @@
           {
             path: '/tisc/adminstration/documentation/agreement-policy-terms',
             name: 'terms.policy',
-            component: './Welcome',
+            component:
+              './TISC/Adminstration/Documentation/AgreementPolicies/CreateAgreementPoliciesPage',
           },
           {
             path: '/tisc/adminstration/documentation/how-to',
