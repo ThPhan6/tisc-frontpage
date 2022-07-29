2d5c8d4301b772005723db375f8ab12cabbd68b7
VuongPham96 <vuongd36@gmail.com>
2022-07-29 14:55:44 +0700

    fix bug collapse box-shadow

--- src/components/Product/SpecificationAttribute.tsx
+++ src/components/Product/SpecificationAttribute.tsx
@@ -1,14 +1,18 @@
-import { useAppSelector } from '@/reducers';
-import CustomCollapse from '../Collapse';
 import { ReactComponent as ActionRightIcon } from '@/assets/icons/action-right.svg';
-import { BodyText } from '../Typography';
-import styles from './styles/attributes.less';
-import Popover from '../Modal/Popover';
+import { useAppSelector } from '@/reducers';
+import { upperCase } from 'lodash';
 import { useState } from 'react';
+import CustomCollapse from '../Collapse';
+import Popover from '../Modal/Popover';
+import { BodyText } from '../Typography';
 import { GeneralFeatureHeader } from './components/GeneralFeature';
+import styles from './styles/attributes.less';
 
 const SpecificationAttribute = () => {
-  const { specification_attribute_groups } = useAppSelector((state) => state.product.details);
+  const specification_attribute_groups = useAppSelector(
+    (state) => state.product.details.specification_attribute_groups,
+  );
+
   const [visible, setVisible] = useState<boolean>(false);
   const [groupName, setGroupName] = useState<string>('');
 
@@ -18,6 +22,37 @@ const SpecificationAttribute = () => {
     /// show popup
     setVisible(true);
   };
+
+  console.log(specification_attribute_groups);
+
+  // const renderCheckBoxLabel = (item: any) => {
+  //   const { basis } = item;
+  //   ///
+  //   let description = '';
+  //   /// must found basis
+  //   if (basis && basis.id) {
+  //     description = basis.name;
+  //     if (!description) {
+  //       /// only conversion don't have name
+  //       description = `${basis.name_1} - ${basis.name_2}`;
+  //     }
+  //     if (basis && basis.type !== 'Conversions' && basis.type !== 'Text') {
+  //       /// count subs
+  //       description += ` (${basis.subs?.length ?? 0})`;
+  //     }
+  //   }
+
+  //   return (
+  //     <div className={styles.attributeItemCheckBox}>
+  //       <BodyText level={3} customClass="attribute-name">
+  //         {item.name}
+  //       </BodyText>
+  //       <BodyText level={5} fontFamily="Roboto" customClass="attribute-description">
+  //         {description}
+  //       </BodyText>
+  //     </div>
+  //   );
+  // };
 
   return (
     <div className={styles.attributes}>
@@ -48,10 +83,27 @@ const SpecificationAttribute = () => {
             </CustomCollapse>
           );
         })}
-      </div>
 
-      {/* popup */}
-      {visible && <Popover title={groupName} visible={visible} setVisible={setVisible}></Popover>}
+        {/* popup */}
+        {visible && (
+          <Popover
+            title={upperCase(groupName)}
+            visible={visible}
+            setVisible={setVisible}
+            // dropdownRadioList={specification_attribute_groups.map((group) => {
+            //   group.attributes.map((attribute) => {
+            //     attribute.basis_options.map((basis) => {
+            //       return {
+            //         name: basis.name,
+            //         options: basis.,
+            //       };
+            //     });
+            //   });
+            // })}
+            dropDownRadioTitle={(data) => data.name}
+          />
+        )}
+      </div>
     </div>
   );
 };
