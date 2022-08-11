c9a61e5ed203988a814943d4fc0dcea6bc6647bc
locnguyen-es <106217274+locnguyen-es@users.noreply.github.com>
2022-08-11 10:12:18 +0700

    Update UpdatePage.tsx

--- src/pages/Designer/Project/UpdatePage.tsx
+++ src/pages/Designer/Project/UpdatePage.tsx
@@ -13,7 +13,7 @@ import { useParams } from 'umi';
 const ProjectCreatePage: React.FC = () => {
   const params = useParams<{ id: string }>();
   const projectId = params?.id || '';
-  const [selectedTab, setSelectedTab] = useState<ProjectTabKeys>(ProjectTabKeys.productConsidered);
+  const [selectedTab, setSelectedTab] = useState<ProjectTabKeys>(ProjectTabKeys.basicInformation);
   const [project, setProject] = useState<ProjectDetailProps>();
   useEffect(() => {
     if (projectId) {
