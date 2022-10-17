3b0d2e23302ca6aa8422f294c4de34eba2ce6692
VuongPham96 <vuongd36@gmail.com>
2022-10-17 15:57:05 +0700

    refactor inquiry reuest

--- src/components/InquiryRequest/index.tsx
+++ src/components/InquiryRequest/index.tsx
@@ -60,8 +60,9 @@ interface InquiryRequestProps {
 }
 
 const InquiryRequest: FC<InquiryRequestProps> = ({ product, visible, setVisible }) => {
-  const submitButtonStatus = useBoolean(false);
+  const isSubmitted = useBoolean(false);
   const [selectedTab, setSelectedTab] = useState<TabKeys>(TabKeys.inquiry);
+  const InquiryTab = selectedTab === TabKeys.inquiry;
 
   const projectData = useAppSelector((state) => state.project.list);
 
@@ -103,7 +104,7 @@ const InquiryRequest: FC<InquiryRequestProps> = ({ product, visible, setVisible 
 
   const setStyleOnContainerClass = () =>
     getValueByCondition([
-      [selectedTab === TabKeys.inquiry && selectedInquiryFor.length, styles.inputColor],
+      [InquiryTab && selectedInquiryFor.length, styles.inputColor],
       [selectedTab === TabKeys.request && selectedRequestFor.length, styles.inputColor],
     ]);
 
@@ -134,7 +135,7 @@ const InquiryRequest: FC<InquiryRequestProps> = ({ product, visible, setVisible 
 
   // handle onChange title and message
   const onChangeValueInput = (newData: 'title' | 'message', fieldValue: any) => {
-    if (selectedTab === TabKeys.inquiry) {
+    if (InquiryTab) {
       setGeneralInquiryData((prevState) => ({
         ...prevState,
         [newData]: fieldValue,
@@ -152,7 +153,7 @@ const InquiryRequest: FC<InquiryRequestProps> = ({ product, visible, setVisible 
     // delete other-input has empty value
     const selectedOpt = selectedOption.filter((el) => el.label !== '');
 
-    if (selectedTab === TabKeys.inquiry) {
+    if (InquiryTab) {
       setSelectedInquiryFor(selectedOpt);
       setGeneralInquiryData((prevState) => ({
         ...prevState,
@@ -186,7 +187,7 @@ const InquiryRequest: FC<InquiryRequestProps> = ({ product, visible, setVisible 
   };
 
   const handleSubmit = () => {
-    if (selectedTab === TabKeys.inquiry) {
+    if (InquiryTab) {
       switch (true) {
         case generalInquirydata.inquiry_for_ids.length === 0:
           message.error('Inquiry For is required');
@@ -219,21 +220,18 @@ const InquiryRequest: FC<InquiryRequestProps> = ({ product, visible, setVisible 
       }
     }
 
-    const submitForm =
-      selectedTab === TabKeys.inquiry
-        ? createGeneralInquiry(generalInquirydata)
-        : createProjectRequest(projectRequestData);
+    const submitForm = InquiryTab
+      ? createGeneralInquiry(generalInquirydata)
+      : createProjectRequest(projectRequestData);
 
     submitForm.then((isSuccess) => {
       if (isSuccess) {
-        // change button icon
-        submitButtonStatus.setValue(true);
+        isSubmitted.setValue(true);
 
         setTimeout(() => {
-          submitButtonStatus.setValue(false);
-
+          isSubmitted.setValue(false);
           // clear data
-          if (selectedTab === TabKeys.inquiry) {
+          if (InquiryTab) {
             setSelectedInquiryFor([]);
             setGeneralInquiryData({
               ...GENERAL_INQUIRY_DEFAULT_STATE,
@@ -257,9 +255,9 @@ const InquiryRequest: FC<InquiryRequestProps> = ({ product, visible, setVisible 
 
   // render Inquiry For and Request For data
   const renderFor = () => {
-    const labelContent = selectedTab === TabKeys.inquiry ? 'Inquiry For' : 'Request For';
-    const valueSelected = selectedTab === TabKeys.inquiry ? selectedInquiryFor : selectedRequestFor;
-    const option = selectedTab === TabKeys.inquiry ? inquiryForData : requestForData;
+    const labelContent = InquiryTab ? 'Inquiry For' : 'Request For';
+    const valueSelected = InquiryTab ? selectedInquiryFor : selectedRequestFor;
+    const option = InquiryTab ? inquiryForData : requestForData;
     return (
       <FormGroup label={labelContent} required layout="vertical" formClass={styles.formGroup}>
         <CollapseCheckboxList
@@ -267,6 +265,7 @@ const InquiryRequest: FC<InquiryRequestProps> = ({ product, visible, setVisible 
           onChange={onChangeCheckboxListData}
           containerClass={setStyleOnContainerClass()}
           otherInput
+          clearOtherInput={isSubmitted.value}
           options={option}
           placeholder={selectedItem()}
         />
@@ -285,7 +284,7 @@ const InquiryRequest: FC<InquiryRequestProps> = ({ product, visible, setVisible 
       hasBoxShadow
       hasHeight
       placeholder="type message title"
-      value={selectedTab === TabKeys.inquiry ? generalInquirydata.title : projectRequestData.title}
+      value={InquiryTab ? generalInquirydata.title : projectRequestData.title}
       onChange={(e) => onChangeValueInput('title', e.target.value)}
       onDelete={() => onChangeValueInput('title', '')}
     />
@@ -301,9 +300,7 @@ const InquiryRequest: FC<InquiryRequestProps> = ({ product, visible, setVisible 
         borderBottomColor="mono-medium"
         boxShadow
         onChange={(e) => onChangeValueInput('message', e.target.value)}
-        value={
-          selectedTab === TabKeys.inquiry ? generalInquirydata.message : projectRequestData.message
-        }
+        value={InquiryTab ? generalInquirydata.message : projectRequestData.message}
       />
     </FormGroup>
   );
@@ -313,7 +310,7 @@ const InquiryRequest: FC<InquiryRequestProps> = ({ product, visible, setVisible 
       title="INQUIRY/REQUEST"
       visible={visible}
       setVisible={setVisible}
-      submitButtonStatus={submitButtonStatus.value}
+      submitButtonStatus={isSubmitted.value}
       onFormSubmit={handleSubmit}>
       <BrandProductBasicHeader
         image={product.images?.[0] || ''}
@@ -335,7 +332,7 @@ const InquiryRequest: FC<InquiryRequestProps> = ({ product, visible, setVisible 
         onChange={(key) => setSelectedTab(key as TabKeys)}
       />
 
-      {selectedTab === TabKeys.request && (
+      {selectedTab === TabKeys.request ? (
         <CollapseRadioFormGroup
           label="Project Name"
           checked={projectRequestData.project_id}
@@ -354,7 +351,7 @@ const InquiryRequest: FC<InquiryRequestProps> = ({ product, visible, setVisible 
           }
           noDataMessage="No project yet"
         />
-      )}
+      ) : null}
 
       {renderFor()}
       {renderTitle()}
