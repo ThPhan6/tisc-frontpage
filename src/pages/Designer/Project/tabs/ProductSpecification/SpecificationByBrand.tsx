898919f1b1610df37276e026e49a04abecf91007
loc-nguyen-es <loc.nguyen@enablestartup.com>
2022-11-28 18:12:50 +0700

    Update project - custom product

--- src/pages/Designer/Project/tabs/ProductSpecification/SpecificationByBrand.tsx
+++ src/pages/Designer/Project/tabs/ProductSpecification/SpecificationByBrand.tsx
@@ -42,6 +42,7 @@ const SpecificationByBrand: FC<BrandListProps> = ({ projectId }) => {
       sorter: { multiple: 1 },
       isExpandable: true,
       render: (_value, record) => <span>{record.name}</span>,
+      defaultSortOrder: 'ascend',
     },
     {
       title: 'Collection',
