3a27e8037f36e463ab9346a776faef597d3f049f
VuongPham96 <vuongd36@gmail.com>
2022-12-06 13:51:41 +0700

    update code

--- config/config.ts
+++ config/config.ts
@@ -63,7 +63,7 @@ export default defineConfig({
     'border-bottom-light': 'inset 0 -.7px 0 rgba(0, 0, 0, 0.3)',
     'border-left-light': 'inset .7px 0 0 rgba(0, 0, 0, 0.3)',
     'border-right-light': 'inset -.7px 0 0 rgba(0, 0, 0, 0.3)',
-    'border-all-light': '0 0 0 .7px rgba(0, 0, 0, 0.3)',
+    'border-all-light': 'inset 0 0 0 .7px rgba(0, 0, 0, 0.3)',
 
     //font fontFamily
     'font-primary': 'Roboto',
