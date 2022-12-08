d2fe46990507662236dbca2c5056099c4d0cb470
VuongPham96 <vuongd36@gmail.com>
2022-12-08 17:33:49 +0700

    update code

--- src/pages/Designer/Products/CustomLibrary/components/SpecificationTab/index.tsx
+++ src/pages/Designer/Products/CustomLibrary/components/SpecificationTab/index.tsx
@@ -268,8 +268,98 @@ export const SpecificationTab: FC<{
       return null;
     }
 
-    return options.map((option: ProductOptionProps, optionIndex: number) => {
+    const renderHeader = (option: ProductOptionProps, optionIndex: number) => {
       const selectOption = specification.attribute_groups?.find((el) => el.id === option.id);
+
+      if (!viewOnly) {
+        return (
+          <InputGroup
+            horizontal
+            noWrap
+            fontLevel={4}
+            containerClass={styles.content}
+            label={<ScrollIcon />}
+            placeholder="type title eg Colour Rand or Material Options"
+            value={option.title}
+            onChange={onChangeOptionTitle(optionIndex)}
+          />
+        );
+      }
+
+      return (
+        <Row style={{ width: '100%' }} align="middle" justify="space-between">
+          {isPublicPage ? (
+            <RobotoBodyText level={6} customClass="optionLabel">
+              {option.title}
+            </RobotoBodyText>
+          ) : (
+            <Col style={{ paddingLeft: specifying ? 0 : 16 }}>
+              <CustomCheckbox
+                options={[
+                  {
+                    label: <RobotoBodyText level={6}>{option.title}</RobotoBodyText>,
+                    value: optionIndex,
+                  },
+                ]}
+                selected={
+                  selectOption?.isChecked
+                    ? [
+                        {
+                          label: <RobotoBodyText level={6}>{option.title}</RobotoBodyText>,
+                          value: optionIndex,
+                        },
+                      ]
+                    : []
+                }
+                onChange={() => {
+                  if (productId && selectOption?.isChecked) {
+                    const newOptionSpec = {
+                      is_refer_document: specification?.attribute_groups?.length
+                        ? specification.attribute_groups.some(
+                            (el) => el.id !== selectOption.id && el.isChecked,
+                          )
+                        : true,
+                      attribute_groups: specification?.attribute_groups?.length
+                        ? specification.attribute_groups.filter((el) => el.id !== selectOption.id)
+                        : [],
+                    };
+
+                    store.dispatch(
+                      setCustomProductDetail(
+                        specifying && specifiedDetail
+                          ? {
+                              specifiedDetail: {
+                                ...specifiedDetail,
+                                specification: newOptionSpec,
+                              },
+                            }
+                          : { specification: newOptionSpec },
+                      ),
+                    );
+                    if (!specifying) {
+                      selectProductSpecification(productId, {
+                        custom_product: true,
+                        specification: newOptionSpec,
+                      });
+                    }
+                  }
+                }}
+              />
+            </Col>
+          )}
+          <Col>
+            <RobotoBodyText level={6}>({option.items.length})</RobotoBodyText>
+          </Col>
+          <Col flex="1 1 100px">
+            <div className="flex-end">
+              <RobotoBodyText level={6}>TAG: {option.tag}</RobotoBodyText>
+            </div>
+          </Col>
+        </Row>
+      );
+    };
+
+    return options.map((option: ProductOptionProps, optionIndex: number) => {
       return (
         <CustomCollapse
           key={option.id || optionIndex}
@@ -277,92 +367,7 @@ export const SpecificationTab: FC<{
           showActiveBoxShadow={!specifying}
           noBorder={specifying || (viewOnly && option.use_image)}
           customHeaderClass={styles.optionCollapse}
-          header={
-            viewOnly ? (
-              <Row style={{ width: '100%' }} align="middle" justify="space-between">
-                {isPublicPage ? (
-                  <RobotoBodyText level={6} customClass="optionLabel">
-                    {option.title}
-                  </RobotoBodyText>
-                ) : (
-                  <Col style={{ paddingLeft: specifying ? 0 : 16 }}>
-                    <CustomCheckbox
-                      options={[
-                        {
-                          label: <RobotoBodyText level={6}>{option.title}</RobotoBodyText>,
-                          value: optionIndex,
-                        },
-                      ]}
-                      selected={
-                        selectOption?.isChecked
-                          ? [
-                              {
-                                label: <RobotoBodyText level={6}>{option.title}</RobotoBodyText>,
-                                value: optionIndex,
-                              },
-                            ]
-                          : []
-                      }
-                      onChange={() => {
-                        if (productId && selectOption?.isChecked) {
-                          const newOptionSpec = {
-                            is_refer_document: specification?.attribute_groups?.length
-                              ? specification.attribute_groups.some(
-                                  (el) => el.id !== selectOption.id && el.isChecked,
-                                )
-                              : true,
-                            attribute_groups: specification?.attribute_groups?.length
-                              ? specification.attribute_groups.filter(
-                                  (el) => el.id !== selectOption.id,
-                                )
-                              : [],
-                          };
-
-                          store.dispatch(
-                            setCustomProductDetail(
-                              specifying && specifiedDetail
-                                ? {
-                                    specifiedDetail: {
-                                      ...specifiedDetail,
-                                      specification: newOptionSpec,
-                                    },
-                                  }
-                                : { specification: newOptionSpec },
-                            ),
-                          );
-                          if (!specifying) {
-                            selectProductSpecification(productId, {
-                              custom_product: true,
-                              specification: newOptionSpec,
-                            });
-                          }
-                        }
-                      }}
-                    />
-                  </Col>
-                )}
-                <Col>
-                  <RobotoBodyText level={6}>({option.items.length})</RobotoBodyText>
-                </Col>
-                <Col flex="1 1 100px">
-                  <div className="flex-end">
-                    <RobotoBodyText level={6}>TAG: {option.tag}</RobotoBodyText>
-                  </div>
-                </Col>
-              </Row>
-            ) : (
-              <InputGroup
-                horizontal
-                noWrap
-                fontLevel={4}
-                containerClass={styles.content}
-                label={<ScrollIcon />}
-                placeholder="type title eg Colour Rand or Material Options"
-                value={option.title}
-                onChange={onChangeOptionTitle(optionIndex)}
-              />
-            )
-          }>
+          header={renderHeader(option, optionIndex)}>
           {viewOnly ? null : (
             <div className="flex-between" style={{ padding: '10px 16px' }}>
               <div
