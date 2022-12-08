28780466ac4330a9689e8774d79ed15debb4dd38
VuongPham96 <vuongd36@gmail.com>
2022-12-08 17:14:05 +0700

    bug fix custom product UI

--- src/pages/Designer/Products/CustomLibrary/components/SpecificationTab/index.tsx
+++ src/pages/Designer/Products/CustomLibrary/components/SpecificationTab/index.tsx
@@ -280,61 +280,67 @@ export const SpecificationTab: FC<{
           header={
             viewOnly ? (
               <Row style={{ width: '100%' }} align="middle" justify="space-between">
-                <Col style={{ paddingLeft: specifying ? 0 : 16 }}>
-                  <CustomCheckbox
-                    options={[
-                      {
-                        label: <RobotoBodyText level={6}>{option.title}</RobotoBodyText>,
-                        value: optionIndex,
-                      },
-                    ]}
-                    selected={
-                      selectOption?.isChecked
-                        ? [
-                            {
-                              label: <RobotoBodyText level={6}>{option.title}</RobotoBodyText>,
-                              value: optionIndex,
-                            },
-                          ]
-                        : []
-                    }
-                    onChange={() => {
-                      if (productId && selectOption?.isChecked) {
-                        const newOptionSpec = {
-                          is_refer_document: specification?.attribute_groups?.length
-                            ? specification.attribute_groups.some(
-                                (el) => el.id !== selectOption.id && el.isChecked,
-                              )
-                            : true,
-                          attribute_groups: specification?.attribute_groups?.length
-                            ? specification.attribute_groups.filter(
-                                (el) => el.id !== selectOption.id,
-                              )
-                            : [],
-                        };
-
-                        store.dispatch(
-                          setCustomProductDetail(
-                            specifying && specifiedDetail
-                              ? {
-                                  specifiedDetail: {
-                                    ...specifiedDetail,
-                                    specification: newOptionSpec,
-                                  },
-                                }
-                              : { specification: newOptionSpec },
-                          ),
-                        );
-                        if (!specifying) {
-                          selectProductSpecification(productId, {
-                            custom_product: true,
-                            specification: newOptionSpec,
-                          });
-                        }
+                {isPublicPage ? (
+                  <RobotoBodyText level={6} customClass="optionLabel">
+                    {option.title}
+                  </RobotoBodyText>
+                ) : (
+                  <Col style={{ paddingLeft: specifying ? 0 : 16 }}>
+                    <CustomCheckbox
+                      options={[
+                        {
+                          label: <RobotoBodyText level={6}>{option.title}</RobotoBodyText>,
+                          value: optionIndex,
+                        },
+                      ]}
+                      selected={
+                        selectOption?.isChecked
+                          ? [
+                              {
+                                label: <RobotoBodyText level={6}>{option.title}</RobotoBodyText>,
+                                value: optionIndex,
+                              },
+                            ]
+                          : []
                       }
-                    }}
-                  />
-                </Col>
+                      onChange={() => {
+                        if (productId && selectOption?.isChecked) {
+                          const newOptionSpec = {
+                            is_refer_document: specification?.attribute_groups?.length
+                              ? specification.attribute_groups.some(
+                                  (el) => el.id !== selectOption.id && el.isChecked,
+                                )
+                              : true,
+                            attribute_groups: specification?.attribute_groups?.length
+                              ? specification.attribute_groups.filter(
+                                  (el) => el.id !== selectOption.id,
+                                )
+                              : [],
+                          };
+
+                          store.dispatch(
+                            setCustomProductDetail(
+                              specifying && specifiedDetail
+                                ? {
+                                    specifiedDetail: {
+                                      ...specifiedDetail,
+                                      specification: newOptionSpec,
+                                    },
+                                  }
+                                : { specification: newOptionSpec },
+                            ),
+                          );
+                          if (!specifying) {
+                            selectProductSpecification(productId, {
+                              custom_product: true,
+                              specification: newOptionSpec,
+                            });
+                          }
+                        }
+                      }}
+                    />
+                  </Col>
+                )}
                 <Col>
                   <RobotoBodyText level={6}>({option.items.length})</RobotoBodyText>
                 </Col>
