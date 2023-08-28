72ac43b7f88a288abbfce3d1612f864c939e14b2
ThinhPhan <thinh.phan@enablestartup.com>
2023-08-28 14:47:03 +0700

    update tooltip for connection status

--- src/pages/TISC/Product/Basis/Option/components/linkage/LinkageConnection.tsx
+++ src/pages/TISC/Product/Basis/Option/components/linkage/LinkageConnection.tsx
@@ -29,29 +29,40 @@ export const LinkageConnection = () => {
         </div>
       </div>
       <div>
-        {connectionList.map((item, index) => {
-          return (
-            <div
-              key={index}
-              className="flex-between"
-              style={{ padding: '0 8px 8px', minHeight: 20 }}
-            >
-              <BodyText fontFamily="Roboto" level={6}>
-                {rootSubItemProductId} to {item.productId}
-              </BodyText>
+        {!connectionList.length ? (
+          <BodyText
+            level={6}
+            fontFamily="Roboto"
+            color="mono-color-medium"
+            style={{ padding: '0 8px' }}
+          >
+            select the product from left
+          </BodyText>
+        ) : (
+          connectionList.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className="flex-between"
+                style={{ padding: '0 8px 8px', minHeight: 20 }}
+              >
+                <BodyText fontFamily="Roboto" level={6}>
+                  {rootSubItemProductId} to {item.productId}
+                </BodyText>
 
-              {chosenOptionIds.includes(item.pairId) ? (
-                <div className="cursor-pointer" onClick={handleChangeConnectionStatus(item)}>
-                  {item.isPair ? <PairIcon /> : <UnPairIcon />}
-                </div>
-              ) : (
-                <div>
-                  <InactivePairIcon />
-                </div>
-              )}
-            </div>
-          );
-        })}
+                {chosenOptionIds.includes(item.pairId) ? (
+                  <div className="cursor-pointer" onClick={handleChangeConnectionStatus(item)}>
+                    {item.isPair ? <PairIcon title="pair" /> : <UnPairIcon title="unpair" />}
+                  </div>
+                ) : (
+                  <div>
+                    <InactivePairIcon title="inactive" />
+                  </div>
+                )}
+              </div>
+            );
+          })
+        )}
       </div>
     </Col>
   );
