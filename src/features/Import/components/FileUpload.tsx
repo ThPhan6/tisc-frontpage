094a4c423a73971c72f1b5f97814e344d87c652f
Linh Nguyen <linhnh.260601@gmail.com>
2024-12-05 14:22:25 +0700

    chore(upload): Add warning instruction when uploading file.

--- src/features/Import/components/FileUpload.tsx
+++ src/features/Import/components/FileUpload.tsx
@@ -7,7 +7,7 @@ import { ReactComponent as UploadIcon } from '@/assets/icons/upload-48.svg';
 import { useImport } from '../hooks/useImport';
 
 import CustomButton from '@/components/Button';
-import { BodyText } from '@/components/Typography';
+import { BodyText, RobotoBodyText } from '@/components/Typography';
 
 const { Dragger } = Upload;
 
@@ -54,6 +54,22 @@ export const FileUpload: FC<FileUploadProps> = ({ ...props }) => {
         >
           Choose file
         </CustomButton>
+      </div>
+
+      <div style={{ transform: 'translateY(150px)' }}>
+        <RobotoBodyText level={4} customClass="font-medium red-magenta">
+          Warning Instruction
+        </RobotoBodyText>
+        <RobotoBodyText level={5} customClass="red-magenta">
+          <span className="block">1. All CSV files should be prepared ahead of the importing.</span>
+          <span className="block">
+            2.Ensure the correct product code (SKU) column list is included.
+          </span>
+          <span className="block">
+            3. System will overwrite all figures, including “0”, from your CSV table cell.
+          </span>
+          4. If not sure, please consult Team TISC at hello@tisc.global.
+        </RobotoBodyText>
       </div>
     </Dragger>
   );
